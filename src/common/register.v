fba9655641080823699cc0a92aa9c3309540fefe
Joe Driscoll <32208193+jwd0023@users.noreply.github.com>
2019-03-01 10:56:29 -0600

    Add files via upload

---
+++ src/common/register.v
@@ -0,0 +1,13 @@
+/* File: register.v
+ * 
+ * Implements a generic register model.
+ *
+ */
+ 
+
+module register #(parameter SIZE = 8) 
+                 (input  [(SIZE-1):0] data_in,
+                  output [(SIZE-1):0] data_out
+                  input               clock);
+
+endmodule
---
+++ src/controller/control_unit.v
@@ -0,0 +1,24 @@
+/* File: control_unit.v
+ *
+ * - Implements the controller used in the CPU design. Essentially
+ *   coordinates all of the control signals for datapath.
+ *
+ */
+
+
+/* Module for the control unit.
+ *  
+ * - Inputs:
+ *     - op_code: 4 bit opcode from instruction 
+ *
+ *
+ *
+ *
+ *
+ */
+module control_unit (input [3:0] op_code,
+                     input [1:0] cond);
+ 
+ 
+ 
+endmodule
